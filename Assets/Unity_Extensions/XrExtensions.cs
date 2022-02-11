006901481f5b96b5d66abfaee41b22982c217521
Reddevildragg <Thomas.greener@hotmail.co.uk>
2022-02-11 15:21:11 +0000

    Updates to vr extensions

---
+++ Assets/Unity_Extensions/XrExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+namespace GG.Extensions
+{
+    public static class XrExtensions
+    {
+        public static bool IsHeadsetPresent()
+        {
+            var xrDisplaySubsystems = new List<XRDisplaySubsystem>();
+            SubsystemManager.GetInstances<XRDisplaySubsystem>(xrDisplaySubsystems);
+            foreach (var xrDisplay in xrDisplaySubsystems)
+            {
+                if (xrDisplay.running)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
