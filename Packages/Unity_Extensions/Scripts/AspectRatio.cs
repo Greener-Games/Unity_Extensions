3242bacef4cae17391609ffc5b90924b47c003d7
Tom <thomas.greener@hotmail.co.uk>
2024-07-03 16:42:13 +0100

    added namespaces

--- Packages/Unity_Extensions/Scripts/AspectRatio.cs
+++ Packages/Unity_Extensions/Scripts/AspectRatio.cs
@@ -1,25 +1,29 @@
 using UnityEngine;
 using System.Collections;
 
-public static class AspectRatio
+namespace GG.Extensions
 {
-    public static Vector2 GetAspectRatio(int x, int y, bool debug = false)
-	{
-        return GetAspectRatio(new Vector2(x,y), debug);
-    }
-	
-    public static Vector2 GetAspectRatio(Vector2 xy, bool debug =false)
-	{
-        float f = xy.x / xy.y;
-        int i = 0;
-        while(true){
-            i++;
-            if(System.Math.Round(f * i, 2) == Mathf.RoundToInt(f*i))
-                break;
+    public static class AspectRatio
+    {
+        public static Vector2 GetAspectRatio(int x, int y, bool debug = false)
+        {
+            return GetAspectRatio(new Vector2(x, y), debug);
         }
-        if(debug)
-            Debug.Log("Aspect ratio is "+ f * i+":"+ i +" (Resolution: "+ xy.x +"x"+ xy.y +")");
-        return new Vector2((float)System.Math.Round(f * i, 2), i);
+
+        public static Vector2 GetAspectRatio(Vector2 xy, bool debug = false)
+        {
+            float f = xy.x / xy.y;
+            int i = 0;
+            while (true)
+            {
+                i++;
+                if (System.Math.Round(f * i, 2) == Mathf.RoundToInt(f * i))
+                    break;
+            }
+
+            if (debug)
+                Debug.Log("Aspect ratio is " + f * i + ":" + i + " (Resolution: " + xy.x + "x" + xy.y + ")");
+            return new Vector2((float)System.Math.Round(f * i, 2), i);
+        }
     }
 }
-
